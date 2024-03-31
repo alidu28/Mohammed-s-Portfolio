123c665a5771906a97b1d611ff2c00106b79d672
alidu28 <mugabejunior5@gmail.com>
2024-03-31 00:05:12 +0000

    my_first_commit

---
+++ my-portfolio/src/components/Hero/Hero.jsx
@@ -0,0 +1,15 @@
+import "./Hero.css"
+
+function Hero() {
+  return (
+    <div className="hero-contain">
+        <h3>I`m a</h3>
+        <h1>
+            FULL STACK SOFTWARE DEVELOPER
+            <div className="dot"></div>
+        </h1>
+        <button className="btn prev-btn">Previous Projects</button>
+    </div>
+  )
+}
+export default Hero
---
+++ my-portfolio/src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import Hero from "../../components/Hero/Hero"
+import About from "../../components/About/About"
+import Project from "../../components/Project/Project"
+
+function Home() {
+  return (
+    <div>
+      <Hero />
+      <About />
+      <Project />
+    </div>
+  ) 
+}
+export default Home
